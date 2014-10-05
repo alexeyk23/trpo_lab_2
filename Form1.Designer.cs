bcf852b5b6493eb23d30f10912a4ca305ef9d96b
alexeyk23 <kunak94@mail.ru>
2014-10-05 22:40:47 +0300

    lab 2

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace trpo_lab_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txbxInpSeq = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txbxRes = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите последовательность";
+            // 
+            // txbxInpSeq
+            // 
+            this.txbxInpSeq.Location = new System.Drawing.Point(12, 25);
+            this.txbxInpSeq.Name = "txbxInpSeq";
+            this.txbxInpSeq.Size = new System.Drawing.Size(416, 20);
+            this.txbxInpSeq.TabIndex = 2;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(434, 25);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 3;
+            this.btnGo.Text = "Посчитать";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // txbxRes
+            // 
+            this.txbxRes.Location = new System.Drawing.Point(12, 109);
+            this.txbxRes.Multiline = true;
+            this.txbxRes.Name = "txbxRes";
+            this.txbxRes.Size = new System.Drawing.Size(416, 64);
+            this.txbxRes.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Результаты";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 185);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txbxRes);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.txbxInpSeq);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txbxInpSeq;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txbxRes;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace trpo_lab_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitLabels();
+        }
+        void InitLabels()        
+        {
+            
+            ansLabels = new Label[maxSize];
+            int x = 12;
+            int y = 50;
+            int dx = 30;
+            for (int i = 0; i < maxSize; i++)
+            {
+                ansLabels[i] = new Label();
+                ansLabels[i].Location = new Point(x, y);
+                ansLabels[i].Width = 33;
+                ansLabels[i].Font = new Font("Microsoft Sans Serif", 14F);
+                ansLabels[i].ForeColor = Color.Black;
+                x = x + dx;
+            }
+            
+            this.Controls.AddRange(ansLabels);  
+        }
+        void clearLabels()
+        {
+            for (int i = 0; i < maxSize; i++)
+            {
+                ansLabels[i].ForeColor = Color.Black;
+                ansLabels[i].Text = "";
+            }
+        }
+        int maxSize = 23;
+        Label[] ansLabels;
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            clearLabels();
+            txbxRes.Text = "";
+            char[] sep = new char[] { ' ',',' };
+            string[] s =      txbxInpSeq.Text.Split(sep,StringSplitOptions.RemoveEmptyEntries);
+            int[] a = new int[s.Length];
+            for (int i = 0; i < s.Length; i++)
+            {
+                a[i] = int.Parse(s[i]);
+            }
+            int start = 0, minLen = 0,start2 = 0,minLen2 =0 ;
+            //замер времени
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            int maxSum = linerAlgo(a, ref start, ref minLen);
+            stopWatch.Stop();            
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime1 = String.Format("{0:f7}", ts.TotalSeconds);
+            stopWatch.Reset();
+            stopWatch.Start();
+            int cubMax = cubixAlgo(a, ref start2, ref minLen2);
+            stopWatch.Stop();
+            ts = stopWatch.Elapsed;
+            string elapsedTime2 = String.Format("{0:f7}",ts.TotalSeconds);
+            string answer = "{0} алгоритм: MAXSUMM = {1}, MINLEN = {2}, время работы = {3}";
+            txbxRes.Text += "Длина последовательности = " + a.Length+Environment.NewLine;
+            txbxRes.Text+=string.Format(answer, "Кубический", cubMax, minLen2, elapsedTime2)+Environment.NewLine;
+            txbxRes.Text += string.Format(answer, "Линейный", maxSum, minLen, elapsedTime1);
+            //выделение цветом
+            for (int i = 0; i < a.Length; i++)
+            {   
+                ansLabels[i].Text = a[i].ToString();
+                if (i >= start && i < start + minLen)                
+                    ansLabels[i].ForeColor = Color.Red;               
+            }
+          
+        }
+        private int cubixAlgo(int[] a, ref int start, ref int minlen)
+        {
+            int sum = 0, maxsum = 0, len = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = i; j < a.Length; j++)
+                {
+                    len = 0;
+                    sum = 0;
+                    for (int k = i; k <= j; k++)
+                    {
+                        sum += a[k];
+                        len++;
+                    }
+                    if (maxsum < sum)
+                    {
+                        maxsum = sum;
+                        start = i;
+                        minlen = len;
+                    }
+                    else
+                        if (sum == maxsum && len < minlen)
+                        {
+                            minlen = len;
+                            start = i;
+                        }
+
+                }
+            }
+            return maxsum;
+        }
+        private int linerAlgo(int[] a,ref int start,ref int minlen)
+        {
+            int i=0;
+            int sum = 0, maxsum = 0, len = 0;
+            while (i < a.Length)
+            {
+                sum += a[i];
+                if (sum <= 0)//состояние ненакопления
+                {
+                    sum = 0;
+                    len = 0;
+                }
+                else
+                {
+                    len++;//накапливаем длинну
+                    if (sum > maxsum)//если надо изменить,
+                    {
+                        minlen = len;
+                        start = i - minlen + 1;//определяем откуда стартовали
+                        maxsum = sum;
+                    }
+                    else
+                        if (sum == maxsum && len < minlen)//если сумма такая же, то смотрим может меньше длина
+                        {
+                            minlen = len;
+                            start = i - minlen + 1;
+                        }
+                }
+                i++;
+            }
+            return maxsum;
+        }
+    }
+}
